94205111aaf50e118b77f552d5fa491408728c4b
fanying2015 <fanyingtang@gmail.com>
2017-08-16 22:34:13 -0400

    Update Battleship.py

--- Battleship.py
+++ Battleship.py
@@ -1,6 +1,6 @@
 # Battle Ship Game
 
-""" In this project you will build a simplified, one-player version of the classic board game Battleship! In this version of the game, there will be a single ship hidden in a random location on a 5x5 grid. The player will have 4 guesses to try to sink the ship."""
+""" In this project a simplified, one-player version of the classic board game Battleship will be built! In this version of the game, there will be a single ship hidden in a random location on a 5x5 grid. The player will have 4 guesses to try to sink the ship."""
 
 # generate an empty board
 from random import randint
